85ccf8c7690b69d079955162fe23662941bb92e8
PeterDaish <965261@swansea.ac.uk>
2021-08-10 20:22:57 +0100

    removed OG database code form settings

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -81,13 +81,13 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-     }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#
+#     }
+#}
 
 if DEVELOPMENT_MODE is True:
     DATABASES = {
