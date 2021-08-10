475021169f3ad335a28aaddc52da483be65ee246
PeterDaish <45074795+PeterDaish@users.noreply.github.com>
2021-08-11 00:24:58 +0100

    changed database back and static

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -81,27 +81,27 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#
-#     }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
+     }
+}
+
+#if DEVELOPMENT_MODE is True:
+    #DATABASES = {
+     #   "default": {
+    #        "ENGINE": "django.db.backends.sqlite3",
+   #         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+  #      }
+ #   }
+#elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    #if os.getenv("DATABASE_URL", None) is None:
+   #     raise Exception("DATABASE_URL environment variable not defined")
+  #  DATABASES = {
+ #       "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+#    }
 
 
 # Password validation
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
